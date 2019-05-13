b878c86873dc97ab47813e08f74657a94f3e805d
Seraphic <seraphic@Seraphics-MacBook-Pro.local>
2019-05-13 18:43:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/libapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.libapp;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void goToLib(View view) {
+        //startActivityForResult(new Intent(this, LibActivity.class), 101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==101)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Toast.makeText(this, data.getStringExtra("result"), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
